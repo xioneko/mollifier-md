6138c62db26b65c0712886ab7945ddc148054bea
Cheni <xioneko@outlook.com>
2025-03-24 22:57:39 +0800

    feat: 公式快捷输入需通过空格触发

--- packages/editor/src/plugins/Equation/inputRules.ts
+++ packages/editor/src/plugins/Equation/inputRules.ts
@@ -6,15 +6,16 @@ export const inputRules: InputRule[] = [
   {
     type: "node",
     transform(node, offset, editor) {
-      if (offset < 3) return false
+      if (offset < 4) return false
       const text = node.__text
-      if (text[offset - 1] !== "$" || text[offset - 2] === " ") return false
+      if (text[offset - 1] !== " " || text[offset - 2] !== "$" || text[offset - 3] === " ")
+        return false
 
-      const start = text.lastIndexOf("$", offset - 3)
+      const start = text.lastIndexOf("$", offset - 4)
       if (start === -1 || text[start + 1] === " ") return false
 
       editor.update(() => {
-        const equation = text.slice(start + 1, offset - 1)
+        const equation = text.slice(start + 1, offset - 2)
         const textNodes = node.splitText(start, offset)
         const equationNode = $createInlineEquationNode(equation)
         if (start === 0) {
@@ -28,14 +29,14 @@ export const inputRules: InputRule[] = [
   {
     type: "node",
     transform(node, offset, editor) {
-      if (offset !== 2 || !node.__text.startsWith("$$")) return false
+      if (offset !== 3 || !node.__text.startsWith("$$ ")) return false
       const block = node.getParent()
       if (
         !($isParagraphNode(block) || block?.canInsertTextBefore() === false) ||
         block.__first !== node.__key
       )
         return false
-      const equation = block.getTextContent().slice(2)
+      const equation = block.getTextContent().slice(3)
       editor.update(() => {
         const equationBlock = $createEquationBlockNode(equation)
         block.replace(equationBlock)
